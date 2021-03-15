d1a6b4549fe25e93871527b6f01089e5b9d1e88c
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2021-03-14 22:09:02 -0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Roberto Edgar Geiss on 14/03/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
